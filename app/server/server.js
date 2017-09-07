5e08b363fc85263c6b1d99243a088832ed447473
Camille <shawn.koga@aggiemail.usu.edu>
2017-09-07 12:10:56 -0600

    added auth stuff

--- app/server/server.js
+++ app/server/server.js
@@ -9,17 +9,62 @@ const express = require('express')
 
 const app = express();
 
+//MIDDLEWARE
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true
-}));
+}))
+
 
 app.use(passport.initialize());
+app.use(passport.session());
 
-massive(process.env.CONNECTIONSTRING).then( db => {
+
+
+//DATABASE CONNECTION
+massive(process.env.CONNECTIONSTRING).then(db => {
     app.set('db', db);
 })
+
+//AUTHENTICATION
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK
+}, function (accessToken, refreshToken, extraParams, profile, done) {
+    const db = app.get('db')
+
+    db.find_user(profile.id).then(user => {
+        console.log(user)
+        if (user[0]) {
+            return done(null, user);
+        } else {
+            db.create_user([profile.displayName, profile.emails[0].value, profile.picture, profile.id])
+                .then(user => {
+                    return done(null, user[0]);
+                })
+        }
+    })
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user)
+})
+
+passport.deserializeUser(function (user, done) {
+    app.get('db').find_session_user(user[0].id).then(user => {
+            return done(null, user[0]);
+        })
+})
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/#/private',
+    failureRedirect: 'http://localhost:3000/#/'
+}))
+
 
 
 let PORT = 3040;
